b28c1b17f03a9739dfdd9633888b92e7c10f6cf0
Kevin Simper <kevin.simper@gmail.com>
2018-08-18 23:19:12 +0200

    Fix bug that template had moved folder

--- prepare.js
+++ prepare.js
@@ -2,7 +2,7 @@ const { readFileSync, writeFileSync } = require('fs')
 const name = process.argv[2].split(' ')
 const first = name[0]
 const last = (name.length > 1) ? name[name.length - 1] : ''
-let content = readFileSync('./template.html').toString()
+let content = readFileSync('./label/template.html').toString()
 content = content.replace('##FIRSTNAME##', first)
 content = content.replace('##LASTNAME##', last)
-writeFileSync('./label.html', content)
+writeFileSync('./label/label.html', content)
