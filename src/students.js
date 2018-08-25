adeb6954b8a209accb25393180f1819a3bc3243a
Kevin Simper <kevin.simper@gmail.com>
2018-08-25 23:56:13 +0200

    Add students

--- src/students.js
+++ src/students.js
@@ -1,12 +1,79 @@
 module.exports = `{
   "class04": [
-    "Kevin"
+    "Daryan",
+    "Isaac",
+    "Ismail",
+    "Rashid",
+    "Yinan"
   ],
   "class05": [
-    "Kevin"
+    "Anowar",
+    "Chandu",
+    "Hedayat",
+    "Jackline",
+    "Majd",
+    "Majid",
+    "Matthew",
+    "Richard",
+    "Shaza",
+    "Sirin"
   ],
   "class06": [
-    "Kevin"
+    "Anas",
+    "Anuradha",
+    "Azizul",
+    "Marco",
+    "Mohamed",
+    "Mohammad",
+    "Rahman",
+    "Samara",
+    "Saqib",
+    "Vignesh",
+    "Zeeshan",
+    "Zoey",
+    "Zuhair"
+  ],
+  "class07": [
+    "Abdulhamid",
+    "Abdulrahman",
+    "Amira",
+    "Badr",
+    "Gerlie",
+    "Johanna",
+    "Martin",
+    "Marwan",
+    "Monya",
+    "Nadiya",
+    "Netabay",
+    "Omid",
+    "Pratiksha",
+    "Quan",
+    "Sofia",
+    "Yana"
+  ],
+  "mentors": [
+    "Abed Sujan",
+    "Ahmed Magdy",
+    "Alicia Gonzales",
+    "Benjamin Dalsgaard Hughes",
+    "Emil Bay",
+    "Fehime Seven",
+    "Jacob Andresen",
+    "Jakub Cioslowski",
+    "Kevin Simper",
+    "Marta Matos",
+    "Martin Clemmensen",
+    "Mikael Wistoft-Ibsen",
+    "Moritz Beber",
+    "Nynne Just Christofferson",
+    "Pachito Marco Calabrese",
+    "Pankaj Kumar Singh",
+    "Petru Catana",
+    "Rasmus Jones",
+    "Rohit Sharma",
+    "Svetlana Galkina",
+    "Younes Meliani",
+    "Zaki Wasik"
   ]
 }
 
