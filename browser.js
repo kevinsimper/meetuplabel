bf60feddf232afa24a13b487427a26c643e37d21
Kevin Simper <kevin.simper@gmail.com>
2018-08-26 15:14:23 +0200

    Run chrome in sandbox mode for Chromebook

--- browser.js
+++ browser.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
   await page.goto('http://localhost:9000/label.html');
   await page.setViewport({width: 945, height: 732})
