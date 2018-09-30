c71cd051b9542c95e9a044d121c9d72fe591de7f
Kevin Simper <kevin.simper@gmail.com>
2018-09-30 21:29:50 +0200

    Move testdata

--- raster/src/index.test.js
+++ raster/src/index.test.js
@@ -2,9 +2,9 @@ const raster = require('./')
 const { readFileSync } = require('fs')
 
 test('raster function should be defined', () => {
-  const data = readFileSync('./label.png')
+  const data = readFileSync('./testdata/label.png')
   console.log(data)
-  const raw = readFileSync('./label.bin')
+  const raw = readFileSync('./testdata/label.bin')
   console.log(raw)
   const newdata = raster()
   console.log(newdata)
