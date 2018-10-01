6dea7392b40e18ac1fa1bc25844bb150a9fad6e0
Kevin Simper <kevin.simper@gmail.com>
2018-10-01 02:11:54 +0200

    Convert image into binary

--- raster/src/index.js
+++ raster/src/index.js
@@ -34,8 +34,48 @@ const addMargins = () => {
   return Buffer.from('1b69642300', 'hex')
 }
 
-const addRasterData = () => {
-  return Buffer.from('', 'hex')
+const addRasterData = imagedata => {
+  const { PNG } = require('pngjs')
+  var png = PNG.sync.read(imagedata)
+  // console.log({ png })
+  let data = []
+  for (var y = 0; y < png.height; y++) {
+    for (var x = 0; x < png.width; x++) {
+      var idx = (png.width * y + x) << 2
+      if (png.data[idx] === 255) {
+        data.push(0)
+      } else {
+        data.push(1)
+      }
+    }
+  }
+  // require('fs').writeFileSync('./data3.txt', data)
+
+  let raw = []
+  let frameLength = data.length
+  let rowLength = Math.floor(png.width / 8) * 8
+  let start = 0
+  while (start + rowLength <= frameLength) {
+    let row = data.slice(start, start + rowLength)
+    start = start + rowLength
+    raw.push(Buffer.from('67', 'hex')[0], Buffer.from('00', 'hex')[0])
+    raw.push(row.length / 8)
+    for (let i = 0; i < row.length; i = i + 8) {
+      let kevin =
+        '' +
+        row[i] +
+        row[i + 1] +
+        row[i + 2] +
+        row[i + 3] +
+        row[i + 4] +
+        row[i + 5] +
+        row[i + 6] +
+        row[i + 7]
+
+      raw.push(parseInt(kevin, 2))
+    }
+  }
+  return Buffer.from(raw)
 }
 
 const addPrint = () => {
@@ -54,8 +94,8 @@ module.exports = function(imagedata) {
   data.push(addCutEvery())
   data.push(addExpandedMode())
   data.push(addMargins())
-  // data.push(addRasterData())
-  // data.push(addPrint())
+  data.push(addRasterData(imagedata))
+  data.push(addPrint())
 
   return Buffer.concat(data)
 }
