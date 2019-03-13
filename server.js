d3c35ae09a4635a1b5a462ce5d5f3b03e04b897a
Kevin Simper <kevin.simper@gmail.com>
2019-03-13 02:54:53 +0100

    Wait for the answers and start the server afterwards

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { readFileSync, writeFileSync } = require('fs')
 const { execSync, exec } = require('child_process')
-const main = require('./src/main.bs')
+const mainPage = require('./src/main.bs')
 const ConfigPage = require('./src/ConfigPage.bs')
 const cors = require('cors')
 const prompts = require('prompts')
@@ -66,51 +66,57 @@ const askUrl = async () => {
   }
 }
 
-let app = express()
-app.use(express.static('./label'))
-app.use(cors())
-app.get('/', (req, res) => {
-  res.send(main.output())
-})
+const main = async () => {
+  await askEvent()
+  await askUrl()
+  let app = express()
+  app.use(express.static('./label'))
+  app.use(cors())
+  app.get('/', (req, res) => {
+    res.send(mainPage.output())
+  })
 
-app.get('/config', (req, res) => {
-  let config = JSON.parse(readFileSync('./config.json'))
-  res.send('<!DOCTYPE html>' + ConfigPage.render(config))
-})
+  app.get('/config', (req, res) => {
+    let config = JSON.parse(readFileSync('./config.json'))
+    res.send('<!DOCTYPE html>' + ConfigPage.render(config))
+  })
 
-app.get('/config/save', (req, res) => {
-  writeFileSync('./config.json', JSON.stringify(req.query, null, 2))
-  res.redirect('/config')
-})
+  app.get('/config/save', (req, res) => {
+    writeFileSync('./config.json', JSON.stringify(req.query, null, 2))
+    res.redirect('/config')
+  })
 
-app.get('/test', (req, res) => {
-  const data = { name: req.query.name }
-  console.log(data)
-  res.send(data)
-})
+  app.get('/test', (req, res) => {
+    const data = { name: req.query.name }
+    console.log(data)
+    res.send(data)
+  })
 
-app.get('/print', (req, res) => {
-  console.log('Prepare')
-  execSync(`node prepare.js "${req.query.name}"`)
-  console.log('Browser')
-  try {
-    exec('node browser.js', {
-      shell: '/bin/bash',
-    })
-  } catch (e) {
-    console.log(e)
-  }
-  setTimeout(() => {
-    console.log('Convert')
-    execSync('bash convert.sh')
-    console.log('Print')
-    execSync('bash print.sh')
-    console.log('Done')
-    res.send('Done')
-  }, 2000)
-})
+  app.get('/print', (req, res) => {
+    console.log('Prepare')
+    execSync(`node prepare.js "${req.query.name}"`)
+    console.log('Browser')
+    try {
+      exec('node browser.js', {
+        shell: '/bin/bash',
+      })
+    } catch (e) {
+      console.log(e)
+    }
+    setTimeout(() => {
+      console.log('Convert')
+      execSync('bash convert.sh')
+      console.log('Print')
+      execSync('bash print.sh')
+      console.log('Done')
+      res.send('Done')
+    }, 2000)
+  })
 
-const PORT = process.env.PORT || 9000
-app.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`)
-})
+  const PORT = process.env.PORT || 9000
+  app.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}`)
+  })
+}
+
+main()
