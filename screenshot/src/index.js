758d9d994135331f0df7a1da3e80051918864d91
Kevin Simper <kevin.simper@gmail.com>
2019-04-02 04:02:38 +0200

    Generate a canvas screenshot of label template

--- screenshot/src/index.js
+++ screenshot/src/index.js
@@ -1,3 +1,23 @@
-import html2canvas from "html2canvas";
+import html2canvas from 'html2canvas'
 
-export default html2canvas;
+function fetchTemplate() {
+  return fetch('/label/template.html').then(r => r.text())
+}
+
+function generateCanvas() {
+  return html2canvas(document.querySelector('#capture'), {}).then(canvas => {
+    document.body.appendChild(canvas)
+  })
+}
+
+function appendTemplate(html) {
+  document.querySelector('#capture').innerHTML = html
+}
+
+async function main() {
+  const template = await fetchTemplate()
+  await appendTemplate(template)
+  await generateCanvas()
+}
+
+main()
